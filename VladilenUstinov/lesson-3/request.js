f402a60a181e7d6cf9f61b21d6ea64fd3957bf9b
&nbsp <ustinov.vladilen@gmail.com>
2019-01-24 23:32:08 +0300

    .gitignore is now working

--- VladilenUstinov/lesson-3/request.js
+++
@@ -1,7 +0,0 @@
-const request = require('request');
-
-request('https://flyontime.ru', (err, response, output) => {
-    if (!err && response.statusCode === 200) {
-        console.log(output);
-    }
-});
